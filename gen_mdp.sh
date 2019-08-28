4ebacb062f3e2ee455412c170fb5866d347c8a06
LeoLeLonquer <leolouis.le-lonquer@laposte.net>
2019-08-28 11:04:44 +0200

    Update gen_mdp.sh

--- gen_mdp.sh
+++ gen_mdp.sh
@@ -14,7 +14,7 @@ DICO_FICHIER="./dico/liste_francais.txt"
 NB_MOTS=$1
 LON_MOT=$2
 
-[ -z "$NB_MOTS" ] && NB_MOTS=3 || true
-[ -z "$LON_MOT" ] && LON_MOT=5 || true
+[ ! -z "$NB_MOTS" ] || NB_MOTS=3
+[ ! -z "$LON_MOT" ] || LON_MOT=5
 
 echo $( awk "length(\$0)>$LON_MOT" "${DICO_FICHIER}" | shuf -n $NB_MOTS )
